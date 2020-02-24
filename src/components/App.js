ad712bd7899a04e94cf8783267728e30b3e9d7ca
ihor <igor@example.com>
2020-02-24 15:13:59 +0200

    finish app

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../api/youtube";
-import VideoList from "./VideoList"
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
 
-    state = { videos: [] }
+    state = { videos: [], selectedVideo: null }
+
+    componentDidMount() {
+        this.onTermSubmit('buildings');
+    }
 
     onTermSubmit = async (term) => {
        const res = await youtube.get("/search", {
@@ -13,22 +18,33 @@ class App extends React.Component {
                 q: term
             }
         })
-        this.setState({videos : res.data.items})
+        this.setState({
+            videos : res.data.items,
+            selectedVideo: res.data.items[0]
+        });
     };
 
     onVideoSelect = video => {
-        console.log(video)
+       this.setState({ selectedVideo: video });
     }
 
     render() {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
-                I have {this.state.videos.length} videos
-                <VideoList 
-                    onVideoSelect={this.onVideoSelect}
-                    videos={this.state.videos}
-                />
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail video={this.state.selectedVideo} />
+                        </div>
+                        <div className="five wide column">
+                            <VideoList 
+                                onVideoSelect={this.onVideoSelect}
+                                videos={this.state.videos}
+                            />  
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
